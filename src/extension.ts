a7a67922a09ffc79dea427d17f888dd279470d34
Aziz Becha <aziz07becha@gmail.com>
2022-11-11 16:02:33 +0100

    added initial command and its function

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,19 @@
 import * as vscode from 'vscode';
+import fetch, { Response } from 'node-fetch';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Congratulations, your extension "vsjoke" is now active!');
-
 	let disposable = vscode.commands.registerCommand('vsjoke.getJoke', () => {
-		vscode.window.showInformationMessage('Hello World from vsjoke!');
+		fetch('https://v2.jokeapi.dev/joke/Programming')
+		.then((response: Response) => response.json())
+  		.then(async (data: any) => {
+			if (data.error === false){
+				const message = `${data.setup}\n${data.delivery}`;
+				const response = await vscode.window.showInformationMessage(message, "Another one");
+				console.log(response);
+			} else {
+				vscode.window.showErrorMessage("An error has been occured");
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
