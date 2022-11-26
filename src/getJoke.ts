e8d9ef12104c76530c42eec67bab35fc1fc462a3
Aziz Becha <aziz07becha@gmail.com>
2022-11-26 21:47:30 +0100

    removed unused console.log

--- src/getJoke.ts
+++ src/getJoke.ts
@@ -5,6 +5,5 @@ const axios = require("axios");
 export const getJoke = async () => {
     const { language, blacklistedFlags } = readSettings();
     const res = await axios.get(`https://v2.jokeapi.dev/joke/Programming?type=twopart&lang=${language}${blacklistedFlags.length > 0 && `&blacklistFlags=${blacklistedFlags}`}`);
-    console.log(`https://v2.jokeapi.dev/joke/Programming?type=twopart&lang=${language}${blacklistedFlags.length > 0 && `&blacklistFlags=${blacklistedFlags}`}`);
     return res.data;
 };
